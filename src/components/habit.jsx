d9e0bda75fd1c2bfc65a2205579dc8098ec9bdc9
aidencoders <aidencoders@gmail.com>
2021-06-02 12:52:32 +0900

    add. use function component, memo

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Habit extends Component {
+class Habit extends PureComponent {
   handleIncrement = () => {
     this.props.onIncrement(this.props.habit);
   };
@@ -11,21 +11,31 @@ class Habit extends Component {
 
   handleDelete = () => {
     this.props.onDelete(this.props.habit);
-  }
+  };
 
   render() {
     const { name, count } = this.props.habit;
+    console.log(`habit: ${name}`);
     return (
       <li className="habit">
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
-        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete" onClick={this.handleDelete}>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
